b1c4980d82808722f5b5fbf459a79ea7f6921ece
Salim <barazasalim24@gmail.com>
2024-07-18 16:12:59 +0300

    wip

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,5 @@
 window.bunilaravel = {
-	baseUrl: 'https://draw-erd-main.test/api',
+	baseUrl: 'http://draw-erd.test:8000/api',
 	isTemplate: false,
     isSample: false,
 	wsUrl: 'socket.getdrawerd.com',
@@ -7,9 +7,9 @@ window.bunilaravel = {
 	localUrl: '{{ url('draw-erd') }}',
 	config: {!! json_encode(config('draw-erd', [])) !!},
 	//project: '1351',
-	project: 'c33e602a-85e4-4584-8b29-57d7e1327b9d',
+	project: '111CA387-8B00-4659-AC70-C5AC0A5FCB4E',
 	//project: '{{ config('draw-erd.project_key') }}',
-	apiKey: '65uaBS40KL4j7q3KPhdkIucoh15g9hK2dHl2Gpij732908cd',
+	apiKey: 'CXdTta1SjM2lUktQeBwLi6PxmLmBpczudXyuCO5lb75018d1',
 	//apiKey: '{{ config('draw-erd.api_key') }}',
 	connections: {!! json_encode(array_keys(config('database.connections', []))) !!},
 	connection: {!! json_encode(config('database.default', null)) !!}
